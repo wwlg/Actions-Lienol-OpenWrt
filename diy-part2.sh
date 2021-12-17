6dab204cfa9bb4f9a2a7dff3a377b0813bae6915
lxhao61 <lxhao61@gmail.com>
2021-12-17 21:11:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,10 +27,10 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 #svn co https://github.com/coolsnowwolf/packages/trunk/net/softethervpn5 feeds/packages/net/softethervpn5
 
 # 删除原版luci-app-softethervpn插件
-rm -rf package/lienol/luci-app-softethervpn
+#rm -rf package/lienol/luci-app-softethervpn
 
 # 拉取修改后的luci-app-softethervpn插件
-svn co https://github.com/lxhao61/openwrt-package/trunk/luci-app-softethervpn package/lienol/luci-app-softethervpn
+#svn co https://github.com/lxhao61/openwrt-package/trunk/luci-app-softethervpn package/lienol/luci-app-softethervpn
 
 # 删除自带xray-core核心
 rm -rf package/feeds/packages/xray-core
