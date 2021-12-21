2553da3c6e882ad861bb4fb6e09b402b209b111d
lxhao61 <lxhao61@gmail.com>
2021-12-22 07:36:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,12 +20,6 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 拉取smartdns核心
 #svn co https://github.com/coolsnowwolf/packages/trunk/net/smartdns package/feeds/packages/smartdns
 
-# 删除原版softethervpn插件
-rm -rf feeds/packages/net/softethervpn
-
-# 拉取softethervpn升级插件
-svn co https://github.com/lxhao61/openwrt-packages/trunk/net/softethervpn feeds/packages/net/softethervpn
-
 # 删除原版softethervpn5插件
 rm -rf feeds/packages/net/softethervpn5
 
@@ -33,10 +27,10 @@ rm -rf feeds/packages/net/softethervpn5
 svn co https://github.com/coolsnowwolf/packages/trunk/net/softethervpn5 feeds/packages/net/softethervpn5
 
 # 删除原版luci-app-softethervpn插件
-rm -rf feeds/lienol/luci-app-softethervpn
+#rm -rf feeds/lienol/luci-app-softethervpn
 
 # 拉取修改后的luci-app-softethervpn插件
-svn co https://github.com/lxhao61/lede/trunk/package/lean/luci-app-softethervpn feeds/lienol/luci-app-softethervpn
+#svn co https://github.com/lxhao61/lede/trunk/package/lean/luci-app-softethervpn feeds/lienol/luci-app-softethervpn
 
 # 删除自带xray-core核心
 rm -rf package/feeds/packages/xray-core
